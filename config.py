4641b5fd7e5f5769ad28e717c3bcd4b1b82cd2b3
Daniil Diveev <69817199+sexozavr@users.noreply.github.com>
2022-01-14 14:05:14 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 API = '1726578435:AAGg-mQXh9gvaQ2TO5JgQm2-Ww1aWYIwOGE'
 num_links_to_get = 10
 path_to_json = "data_1.json"
+USER_DATABASE = 'users.db'
+REQUEST_DATABASE = 'request.db'
--- db_management.py
+++ db_management.py
@@ -1,7 +1,9 @@
 import sqlite3
 import os
+from datetime import datetime
+import numpy as np
 
-def add_record(db_path: str, user_id: int, times_used: int) -> None:
+def add_user_record(db_path: str, user_id: int, times_used: int) -> None:
     if not os.path.exists(db_path): raise FileNotFoundError(f'There is no file {db_path}')
 
     connection = sqlite3.connect(db_path)
@@ -12,9 +14,9 @@ def add_record(db_path: str, user_id: int, times_used: int) -> None:
         USER_ID INTEGER PRIMARY KEY,
         USED_BEFORE INTEGER)''')
 
-        print('Successfully created database USERS')
+        print('Successfully created table USERS')
     except:
-        print('Successfully found database USERS')
+        print('Successfully found table USERS')
 
     cursor.execute(f'''INSERT INTO USERS VALUES(
                     {user_id}, {times_used})''')
@@ -24,18 +26,64 @@ def add_record(db_path: str, user_id: int, times_used: int) -> None:
     print('Record added successfully!')
 
 
-def get_user_stats(db_path: str, user_id: int):
+def get_user_stats(db_path: str, user_id:int) -> list:
     if not os.path.exists(db_path): raise FileNotFoundError(f'There is no file {db_path}')
 
     connection = sqlite3.connect(db_path)
     cursor = connection.cursor()
 
-    cursor.execute(f'''SELECT USED_BEFORE
+    cursor.execute(f'''SELECT *
                        FROM USERS
                        WHERE USER_ID = {user_id}''')
 
     data = cursor.fetchall()
+    connection.close()
 
     return data
+
+def add_request_record(db_path:str, user_id:int, query:str) -> None:
+    if not os.path.exists(db_path): raise FileNotFoundError(f'There is no file {db_path}')
+
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(f'''CREATE TABLE REQUESTS(
+                           USER_ID INTEGER,
+                           REQUSET_ID INTEGER PRIMARY KEY,
+                           TIME DATETIME,
+                           QUERY TEXT)''')
+
+        print('Successfully created REQUESTS table!')
+    except sqlite3.OperationalError:
+        print('Successfully found REQUESTS table!')
+
+    request_id = np.random.randint(0, 10000000)
+
+    cursor.execute('''INSERT INTO REQUESTS VALUES(?, ?, ?, ?)''', (user_id, request_id, datetime.now(), query))
+
+    connection.commit()
+    connection.close()
+
+    print('Record added successfully!')
+
+def update_users_table(db_path:str, user_id:int) -> None:
+    if not os.path.exists(db_path): raise FileNotFoundError(f'There is no file {db_path}')
+
+    user_id, times_used = get_user_stats(db_path, user_id)[0]
+
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+
+    cursor.execute(f'''DELETE FROM USERS WHERE USER_ID = {user_id}''')
+    connection.commit()
+
+    cursor.execute(f'''INSERT INTO USERS VALUES({user_id}, {times_used + 1})''')
+    connection.commit()
+
+    connection.close()
+
+    print('Successfully updated USERS database!')
+
 
 
