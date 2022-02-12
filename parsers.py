b876477f7f1420dd4d1c491bc3c626407deabe09
sexozavr <daniildiveev@mail.ru>
2022-02-12 04:33:21 +0300

    added threading to parsing, changed searching function a bit, having an issue parsing entrepreneur db

--- parsers.py
+++ parsers.py
@@ -2,14 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import json
-from tqdm import tqdm
 from config import NUM_LINKS_TO_GET, PATH_TO_JSON
 from googlesearch import search
 from text_handling import find_most_relevant_part
 import threading
 import wikipedia as wiki
 
-def parse_google(query:str) -> list:
+def parse_google(query:str, results:list) -> list:
 	texts = []
 	links = search(query=query, stop=5, start=0, pause=2)
 
@@ -19,10 +18,10 @@ def parse_google(query:str) -> list:
 		text = BeautifulSoup(html, 'lxml').text
 		texts.append(text)
 
-	return texts
+	results.extend(texts)
 
 
-def nalog_ru_parser(query) -> list:
+def nalog_ru_parser(query:str, results:list) -> list:
 	texts, links = [], []
 
 	url = 'https://www.nalog.gov.ru/rn78/search/?type=all&text=' + query + '&dr=3271&rm=1&type=documents&fd=&td='
@@ -44,35 +43,53 @@ def nalog_ru_parser(query) -> list:
 		text = BeautifulSoup(str(div_el), 'lxml').text
 		texts.append(text)
 
-	return texts
+	results.extend(texts)
 
 
-def search_for_relevant_part_in_json(path_to_json:str, query:str,) -> str:
-    if not os.path.exists(path_to_json): raise FileNotFoundError(f"No file {path_to_json}")
-    if not path_to_json.split(".")[-1] == 'json': raise TypeError(f"Your file {path_to_json} is not .json")
+def search_for_relevant_part_in_json(query:str, results:list) -> str:
+	path_to_json = PATH_TO_JSON
 
-    with open(path_to_json, 'r', encoding='utf=8') as f:
-        parts = json.loads(f.read())
+	if not os.path.exists(path_to_json): raise FileNotFoundError(f"No file {path_to_json}")
+	if not path_to_json.split(".")[-1] == 'json': raise TypeError(f"Your file {path_to_json} is not .json")
 
-    max_sim_tokens = 0
+	with open(path_to_json, 'r', encoding='utf=8') as f:
+		parts = json.loads(f.read())
 
-    for text in tuple(parts.values()):
-        found_part, sim_tokens = find_most_relevant_part(query, text, 32)
+	max_sim_tokens = 0
 
-        if sim_tokens >= max_sim_tokens:
-            max_sim_tokens = sim_tokens
-            best_part = found_part
+	for text in tuple(parts.values()):
+		found_part, sim_tokens = find_most_relevant_part(query, text, 32)
 
-    return best_part, max_sim_tokens
+	if sim_tokens >= max_sim_tokens:
+		max_sim_tokens = sim_tokens
+		best_part = found_part
+
+	results.append((best_part, max_sim_tokens))
 
 
-def wiki_parser(query:str) -> list:
-	page = wiki.search(query)
-	return page.content
+def wiki_parser(query:str, results:list) -> list:
+	wiki.set_lang("ru")
+	search_results = wiki.search(query)
+	page = wiki.page(search_results[0])
+	
+	results.append(page.content)
+
 
 def multiple_parse(query:str) -> list: 
-	json_best_part, _ = search_for_relevant_part_in_json(PATH_TO_JSON, query)
-	return nalog_ru_parser(query) + parse_google(query) +  + [json_best_part]
+	thread_list, results = [], []
+
+	for func in (nalog_ru_parser, search_for_relevant_part_in_json, parse_google, wiki_parser):
+		thread = threading.Thread(target=func, args=(query,results))
+		thread_list.append(thread,)
+		thread.start()
+
+		print(f'Added thread: {func.__name__}')
+
+	for i in range(len(thread_list)):
+		thread_list[i].join()
+
+	return results
+
 
 def get_info_on_specific_entrepreneur(query: str):
 	url = 'https://zachestnyibiznes.ru/search?query='
@@ -85,14 +102,16 @@ def get_info_on_specific_entrepreneur(query: str):
 	html = requests.get(url).text
 
 	soup = BeautifulSoup(html, 'lxml')
-	link = soup.find('a', {'class' : 'f-s-16 no-underline'})
+	link = soup.find('a', {'class' : 'f-s-16'})
 
 	url = 'https://zachestnyibiznes.ru/' + link['href']
 	
 	html = requests.get(url).text
 	soup = BeautifulSoup(html, 'lxml')
 
-	
+	div_el = soup.find('div', {'class' : 'col-md-6'})
+	soup = BeautifulSoup(str(div_el), 'lxml')
 
+	p_els = soup.find_all('p', {'class' : 'm-b-10 no-indent'})
 
-	
+	print(p_els)
