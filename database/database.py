bd8700652dcfd354790f6b8d1fbe1570a2f17c25
Daniil Diveev <daniildiveev@mail.ru>
2022-11-04 16:57:25 +0300

    [MODIFY] add functions for database interactions

--- database/database.py
+++ database/database.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 from typing import List
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker
 from config.config import settings
-from database_models import User, Requests, Base
+from database_models import User, Request, Base
 
 engine = create_engine(
     settings.database_source,
@@ -19,13 +19,35 @@ Session = sessionmaker(
 )
 
 def add_request_record(user_id:int, query:str) -> None:
-    raise NotImplementedError
+    with Session() as session:
+        values = {
+            "user_id" : user_id,
+            "time" : datetime.now(), 
+            "query" : query
+        }
 
-def add_user_record(user_id:int, times_used:int) -> None:
-    raise NotImplementedError
+        new_request = Request(**values)
+        session.add(new_request)
+        session.commit()
+
+def add_user_record(user_id:int, times_used_before:int) -> None:
+    with Session() as session:
+        new_user = User(user_id=user_id, times_used_before=times_used_before)
+        session.add(new_user)
+        session.commit()
 
 def get_user_stats(user_id:int) -> List[str]:
-    raise NotImplementedError
+    with Session() as session:
+        requests = session.query(Request).filter_by(user_id=user_id).all()
 
-def update_users_table(user_id:int) -> None:
-    raise NotImplementedError
+    return requests
+
+
+def update_user_table(user_id:int) -> None:
+    user_id, times_used = get_user_stats(user_id)
+
+    with Session() as session: 
+        session.query(User) \
+               .filter_by(user_id=user_id) \
+               .update({"times_used_before" : times_used + 1})
+        session.commit()
