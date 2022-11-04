a5e32390b45ad4513fe5d477c57724c36555304f
Daniil Diveev <daniildiveev@mail.ru>
2022-11-04 15:39:48 +0300

    [ADD] add file with database models

---
+++ database/database_models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy as sa
+
+Base = declarative_base()
+
+class Requests(Base):
+    __tablename__ = 'requests'
+
+    user_id = sa.Column(sa.INTEGER, foreign_key=True)
+    request_id = sa.Column(sa.INTEGER, primary_key=True)
+    time = sa.Column(sa.DATETIME)
+    query = sa.Column(sa.TEXT)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = sa.Column(sa.INTEGER, primary_key=True)
+    times_used_before = sa.Column(sa.INTEGER)
