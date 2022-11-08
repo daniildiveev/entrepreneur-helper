9c6f0097f5e195c241eb7b61923f4f718ffb5fb3
Daniil Diveev <daniildiveev@mail.ru>
2022-11-08 09:42:41 +0300

    [MODIFY] deleted sqlite files from config

--- modules/setup/config.py
+++ modules/setup/config.py
@@ -11,8 +11,6 @@ USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
 NUM_LINKS_TO_GET = 5
 PATH_TO_JSON = "data_1.json"
-USER_DATABASE = 'users.db'
-REQUEST_DATABASE = 'requests.db'
 QA_MODEL = 'AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru'
 SENTENCE_MODEL = 'symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli'
 SIMILARITY_THRESHOLD = 0.7
