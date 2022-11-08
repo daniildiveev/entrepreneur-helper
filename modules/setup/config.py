0a22624bc82bb1dc8e12e330d00da5909f9816f0
Daniil Diveev <daniildiveev@mail.ru>
2022-11-08 09:44:50 +0300

    [MODIFY] delete json path from config

--- modules/setup/config.py
+++ modules/setup/config.py
@@ -10,7 +10,6 @@ DB_NAME = os.environ.get("DB_NAME")
 USER_PASSWORD = os.environ.get("USER_PASSWORD")
 
 NUM_LINKS_TO_GET = 5
-PATH_TO_JSON = "data_1.json"
 QA_MODEL = 'AlexKay/xlm-roberta-large-qa-multilingual-finedtuned-ru'
 SENTENCE_MODEL = 'symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli'
 SIMILARITY_THRESHOLD = 0.7
